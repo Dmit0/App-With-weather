3d1ba981d8e184b427e16dca545037e658522d2f
Dmitrii Shehunov <53045487+Dmit0@users.noreply.github.com>
2020-07-11 17:26:56 +0300

    Update App.js

--- client - site/src/App.js
+++ client - site/src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import {BrowserRouter as Router}from 'react-router-dom'
 import './css/index.css'
 import {useRoutes} from './routes'
+import {useSelector} from 'react-redux'
+//import {useAuth} from './hooks/auth_hook'
+//import {AuthContext} from './context/AuthContext'
 
 
 function App() {
-  const routes=useRoutes(false)
+ 
+  const isAuthenticated=useSelector((state)=>{
+    return state.auth.token
+  })
+  const routes=useRoutes(!!isAuthenticated)
   return (
     <Router>
       <div>
-      
           {routes}
     </div>
     </Router>
