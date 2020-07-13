62aa8c0e1465ec9a9dd6b9e40ddf6d9386e43786
Dmitrii Shehunov <53045487+Dmit0@users.noreply.github.com>
2020-07-13 23:00:39 +0300

    Update appReducer.js

--- client - site/src/redux/Reducers/appReducer.js
+++ client - site/src/redux/Reducers/appReducer.js
@@ -2,7 +2,7 @@ import {STATES_OF_APPLICATION} from '../types'
 
 const initialState={
     loading:false,
-    errors:false,
+    errors:[],
     show_registration_window:false
 }
 
@@ -13,9 +13,9 @@ export const appReducer=(state=initialState,action)=>{
         case STATES_OF_APPLICATION.HIDE_LOADING:
             return {...state,loading:false}
         case STATES_OF_APPLICATION.SHOW_ERORR:
-            return {...state,errors:true}
-        case STATES_OF_APPLICATION.HIDE_ERORR:
-            return {...state,errors:false}
+            return {...state,errors:[...state.errors,action.payload]}
+        case STATES_OF_APPLICATION.HIDE_ERROR:
+            return {...state,errors:[]}
         case STATES_OF_APPLICATION.CHANGE_STATE_REGIST:
             return {...state,show_registration_window:action.payload.state_regist}
         default:return state
